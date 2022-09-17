1eff7713a82154940230225a63dfa261f79cc2e6
Kota <maedakoutaa@gmail.com>
2022-09-17 13:14:28 +0900

    Initial Commit

---
+++ MiniApp96-ButtonModifier-SwiftUI/MiniApp96_ButtonModifier_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp96_ButtonModifier_SwiftUIApp.swift
+//  MiniApp96-ButtonModifier-SwiftUI
+//
+//  Created by 前田航汰 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp96_ButtonModifier_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
